da9d7b8c7ced2c925fd0ec5a37ed55e5a56a6b48
aereeeee <dofl5576@gmail.com>
2019-05-16 15:31:33 +0900

    fix const

--- src/components/const.js
+++ src/components/const.js
@@ -1,1 +1,1 @@
-export const url = 'http://ec2-52-79-199-251.ap-northeast-2.compute.amazonaws.com:3000/api/cams'
+export const url = 'http://ec2-15-164-67-26.ap-northeast-2.compute.amazonaws.com:3000/api/cams'
